ff49c0dad9a7145ab94915064f3234901e8a6a68
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-12-05 08:23:49 +0300

    Routing ops

--- web-app/src/components/Navbar.js
+++ web-app/src/components/Navbar.js
@@ -1,13 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import {Link} from 'react-router-dom';
 
-export default function AccountMenu() {
+export default function Navbar() {
   return (
     <React.Fragment>
       <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Typography sx={{ minWidth: 100 }}>Harita</Typography>
-        <Typography sx={{ minWidth: 100 }}>İstatistikler</Typography>
+        <Link to="/" ><Typography sx={{ minWidth: 100 }}>Harita</Typography></Link>
+        <Link to="/statistic"><Typography sx={{ minWidth: 100 }}>İstatistikler</Typography></Link>
       </Box>
     </React.Fragment>
   );
