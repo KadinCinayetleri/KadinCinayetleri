674d5a66d1de9e99c3cc8e370cba85f05e05a64f
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-12-15 19:54:53 +0300

    Navbar ui

--- web-app/src/components/Navbar.js
+++ web-app/src/components/Navbar.js
@@ -2,14 +2,23 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import {Link} from 'react-router-dom';
+import { Button, Stack } from '@mui/material';
 
 export default function Navbar() {
+  const style = {
+    color: "#fbe9e7",
+    borderColor: "#fbe9e7",
+    '&:hover': {
+      color: '#ff8a65',
+      borderColor: '#ff7043',
+    },
+  }
   return (
     <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-        <Link to="/" ><Typography sx={{ minWidth: 100 }}>Harita</Typography></Link>
-        <Link to="/statistic"><Typography sx={{ minWidth: 100 }}>İstatistikler</Typography></Link>
-      </Box>
+      <Stack direction="row" spacing={2}>
+        <Button sx={style} href="/" variant="outlined">Harita</Button>
+        <Button sx={style} href="/statistic" variant="outlined">İstatistikler</Button>
+      </Stack>
     </React.Fragment>
   );
 }
